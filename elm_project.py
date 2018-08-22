2517710a38d46d89e6ec4fcf277e850a693538b1
Kevin Yank <thatguy@kevinyank.com>
2018-08-22 19:50:14 +1000

    Remove Elm build configuration commands
    These commands for controlling build settings are not needed for now, since
    support for doing full project builds has been removed from this package.
    
    The one remaining command will now find and open an elm.json file.

--- elm_project.py
+++ elm_project.py
@@ -11,47 +11,7 @@ class ElmProjectCommand(sublime_plugin.TextCommand):
 
     def run(self, edit, prop_name=None, choices=None, caption=None):
         self.window = self.view.window()
-        if not prop_name:
-            self.window.open_file(self.project.json_path, sublime.TRANSIENT)
-            return
-        self.prop_name = prop_name
-        initial_value = getattr(self.project, prop_name)
-        if choices:
-            self.show_choices(choices, initial_value)
-        else:
-            self.window.show_input_panel(caption, initial_value, self.on_finished, None, None)
-
-    def show_choices(self, choices, initial_value):
-        self.norm_choices = [choice.lower() for choice in choices]
-        try:
-            # ValueError: $initial_value is not in list
-            initial_index = self.norm_choices.index(initial_value.lower())
-            # ST2: Boost.Python.ArgumentError: Python argument types
-            self.window.show_quick_panel(choices, self.on_choice, selected_index=initial_index)
-        except: # simplest control flow
-            if not is_ST2():
-                log_string('project.logging.invalid_choice', initial_value)
-            self.window.show_quick_panel(choices, self.on_choice)
-
-    def on_choice(self, index):
-        if index != -1:
-            self.on_finished(self.norm_choices[index])
-
-    def on_finished(self, value):
-        setattr(self.project, self.prop_name, value)
-        keys = self.project._last_updated_key_path
-        if keys:
-            sublime.status_message(get_string('project.updated', '.'.join(keys), value))
-
-BUILD_KEY = ('sublime-build',)
-MAIN_KEY = BUILD_KEY + ('main',)
-HTML_KEY = BUILD_KEY + ('html',)
-OUTPUT_KEY = BUILD_KEY + ('output',)
-OUTPUT_PATH_KEY = OUTPUT_KEY + ('path',)
-OUTPUT_COMP_KEY = OUTPUT_KEY + ('components',)
-OUTPUT_DIR_KEY = OUTPUT_COMP_KEY + ('dir',)
-OUTPUT_NAME_KEY = OUTPUT_COMP_KEY + ('name',)
-OUTPUT_EXT_KEY = OUTPUT_COMP_KEY + ('ext',)
+        self.window.open_file(self.project.json_path, sublime.TRANSIENT)
 
 class ElmProject(object):
 
@@ -59,7 +19,7 @@ class ElmProject(object):
     def find_json(cls, dir_path):
         if not fs.isdir(fs.abspath(dir_path)):
             return None
-        file_path = fs.abspath(fs.join(dir_path, 'elm-package.json'))
+        file_path = fs.abspath(fs.join(dir_path, 'elm.json'))
         if fs.isfile(file_path):
             return file_path
         parent_path = fs.join(dir_path, fs.pardir)
@@ -128,52 +88,3 @@ class ElmProject(object):
     @property
     def working_dir(self):
         return fs.dirname(self.json_path) if self.json_path else None
-
-    @property
-    def main_path(self):
-        return self[MAIN_KEY] or fs.relpath(self.file_path, self.working_dir)
-
-    @main_path.setter
-    def main_path(self, value):
-        self[MAIN_KEY] = value
-
-    @property
-    def html_path(self):
-        return self[HTML_KEY] or self.output_path
-
-    @html_path.setter
-    def html_path(self, value):
-        self[HTML_KEY] = value
-
-    @property
-    def output_path(self):
-        output_path = fs.join(self.output_dir, self.output_name + '.' + self.output_ext)
-        return self[OUTPUT_PATH_KEY] or fs.normpath(output_path)
-
-    @output_path.setter
-    def output_path(self, value):
-        self[OUTPUT_PATH_KEY] = value
-
-    @property
-    def output_dir(self):
-        return self[OUTPUT_DIR_KEY] or 'build'
-
-    @output_dir.setter
-    def output_dir(self, value):
-        self[OUTPUT_DIR_KEY] = value
-
-    @property
-    def output_name(self):
-        return self[OUTPUT_NAME_KEY] or fs.splitext(fs.basename(self.main_path))[0]
-
-    @output_name.setter
-    def output_name(self, value):
-        self[OUTPUT_NAME_KEY] = value
-
-    @property
-    def output_ext(self):
-        return self[OUTPUT_EXT_KEY] or 'html'
-
-    @output_ext.setter
-    def output_ext(self, value):
-        self[OUTPUT_EXT_KEY] = value
